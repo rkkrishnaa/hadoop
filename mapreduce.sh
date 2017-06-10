775eb604a389408105d180b75edae549126b1663
rk <radhakrishnan.r@cloudenablers.com>
2017-06-10 16:54:08 +0530

    mapreduce.sh

--- mapreduce.sh
+++ mapreduce.sh
@@ -1,11 +1,12 @@
 homefolder=$HOME
 a=$(openssl rand -hex 2)
-input_dir=input_dir
+input_dir=input_dir$a
 output_dir=output_dir$a
-mkdir $homefolder/units
-javac -classpath hadoop-core-1.2.1.jar -d units ProcessUnits.java
-jar -cvf units.jar -C units/ .
+echo "hadoop input directory"  $input_dir
+echo "hadoop output directory" $output_dir
+mkdir -p mapred$a/units
+javac -classpath hadoop-core-1.2.1.jar -d mapred$a/units ProcessUnits.java
+jar -cvf units.jar -C mapred$a/units/ .
 hadoop fs -mkdir $input_dir
 hadoop fs -put sample.txt $input_dir
 hadoop jar units.jar hadoop.ProcessUnits $input_dir $output_dir
-echo $output_dir
