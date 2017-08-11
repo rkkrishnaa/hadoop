63fca26c839296577b013dbcd5c06b877584c0fd
Radhakrishnan.R <thrinethratechhara@users.noreply.github.com>
2017-08-11 08:50:05 +0530

    Update spark.sh

--- spark/spark.sh
+++ spark/spark.sh
@@ -6,11 +6,10 @@ NUM_EXECUTORS='3'
 DRIVER_MEMORY='512m'
 EXECUTOR_MEMORY='512m'
 EXECUTOR_CORES='1'
-
+cd $SPARK_HOME
 spark-submit --class $CLASS \
              --master $MASTER \
-             --num-executors $NUM_EXECUTORS \
-             --driver-memory $DRIVER_MEMORY \ 
+             --num-executors $NUM_EXECUTORS --driver-memory $DRIVER_MEMORY  \
              --executor-memory $EXECUTOR_MEMORY \
              --executor-cores $EXECUTOR_CORES \
              $SPARK_HOME/examples/jars/spark-examples*.jar 10
